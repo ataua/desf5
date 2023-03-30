e97a61f7e82333a7099617ab010ad47fdb8fdf6f
ataua <ataua.doederlein@gmail.com>
2023-03-29 21:49:58 -0300

    [FIX] mongo config

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose')
 
 const connect = async () => {
-  const uri = 'mongodb://0.0.0.0:27017/desf5'
-  return await mongoose.connect(
-    uri,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
-  )
+  try {
+    const uri = 'mongodb://0.0.0.0:27017/desf5'
+    return await mongoose.connect(
+      uri,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    )
+  } catch (error) {
+    throw new Error(error.message)
+  }
 }
 
 module.exports = connect
--- src/routes/db.route.js
+++ src/routes/db.route.js
@@ -1,15 +1,16 @@
 const fs = require('fs')
 const path = require('path')
 const db = require('../db/pg')
-
+const connect = require('../db/mongo')
 const { Router } = require('express')
 const { QueryTypes } = require('sequelize')
+const updateMongo = require('../db/update-mongo')
+
 const dbRouter = new Router()
 
-const sqlFilePath = path.join(__dirname, '../../postgres-init-scripts/init-postgres.sql')
-const sql = fs.readFileSync(sqlFilePath, 'utf8')
-
 dbRouter.get('/pg', async (req, res) => {
+  const sqlFilePath = path.join(__dirname, '../../postgres-init-scripts/init-postgres.sql')
+  const sql = fs.readFileSync(sqlFilePath, 'utf8')
   try {
     await db.query(sql, { type: QueryTypes.RAW })
     res.status(201).json({ msg: 'DB PG atualizado.' })
@@ -20,8 +21,16 @@ dbRouter.get('/pg', async (req, res) => {
 
 dbRouter.get('/mongo', async (req, res) => {
   try {
-    await db.query(sql, { type: QueryTypes.RAW })
-    res.status(201).json({ msg: 'DB PG atualizado.' })
+    // const dataFilePath = path.join(__dirname, '../../mongo-init-scripts/init-mongo.js')
+    // const data = fs.readFileSync(dataFilePath, 'utf8')
+    const mongo = await connect()
+    const collections = await mongo.connection.db.listCollections({ name: 'livroInfo' }).toArray()
+    if (collections.length !== 0) {
+      await mongo.connection.dropCollection('livroInfo')
+    }
+    await updateMongo(mongo.connection)
+    await mongo.connection.close()
+    return res.status(201).json({ msg: 'DB Mongo atualizado.' })
   } catch (error) {
     res.status(500).json({ erro: error.message })
   }
