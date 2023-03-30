9a60ffead7102edf532ae3e5141a3ab2d45f2881
ataua <ataua.doederlein@gmail.com>
2023-03-30 17:19:22 -0300

    [UPDATE] autor

--- src/repositories/autor.repository.js
+++ src/repositories/autor.repository.js
@@ -1,20 +1,25 @@
 const Autor = require('../models/autor.model')
 
 const createAutor = async (autor) => await Autor.create(autor)
+
 const getAutor = async (autorId) => await Autor.findByPk(autorId)
-const getAutors = async () => await Autor.findAll({})
+
+const getAutorByEmail = async (email) => await Autor.findOne({ where: { email } })
+
+const getAutores = async () => await Autor.findAll({})
+
 const deleteAutor = async (autorId) => await Autor.destroy({ where: { autor_id: autorId } })
+
 const updateAutor = async (autor) => {
-  const { autor_id } = autor
-  delete autor.autor_id
-  await Autor.update(autor, { where: { autor_id } })
+  await Autor.update(autor, { where: { autor_id: autor.autor_id } })
   return await getAutor(autor.autor_id)
 }
 
 module.exports = {
   createAutor,
   getAutor,
-  getAutors,
+  getAutorByEmail,
+  getAutores,
   deleteAutor,
   updateAutor
 }
--- src/routes/autor.route.js
+++ src/routes/autor.route.js
@@ -2,18 +2,18 @@ const { Router } = require('express')
 const {
   createAutor,
   getAutor,
-  getAutors,
+  getAutorByEmail,
+  getAutores,
   deleteAutor,
   updateAutor
 } = require('../repositories/autor.repository')
 const { param, body, validationResult } = require('express-validator')
-const Autor = require('../models/autor.model')
 
 const autorRouter = Router()
 
-autorRouter.get('/', async (req, res) => {
+autorRouter.get('/', async (_, res) => {
   try {
-    return res.status(200).json(await getAutors())
+    return res.status(200).json(await getAutores())
   } catch (error) {
     return res.status(400).json({ erro: error.message })
   }
@@ -37,34 +37,27 @@ autorRouter.get('/:id',
 
 autorRouter.post('/',
   body('nome').not().isEmpty().trim().escape(),
-  body('email').not().isEmpty().trim().escape().custom(async (email) => {
-    if (await Autor.findOne({ where: { email } })) {
-      Promise.reject(new Error('E-mail já está cadastrado.'))
-    }
-  }),
-  body('senha').not().isEmpty().trim().escape(),
+  body('email').not().isEmpty().trim().escape().isEmail(),
   body('telefone').not().isEmpty().trim().escape(),
-  body('endereco').not().isEmpty().trim().escape(),
   async (req, res) => {
     try {
       const errors = validationResult(req)
       if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
       }
-      const { autor } = req.body
+      const autor = req.body
+      const emailEmUso = await getAutorByEmail(autor.email)
+      if (emailEmUso) {
+        throw new Error('E-mail já cadastrado')
+      }
       return res.status(200).json(await createAutor(autor))
     } catch (error) {
-      return res.status(400).json({ msg: error.messsage })
+      return res.status(400).json({ msg: error.message })
     }
   }
 )
-
 autorRouter.delete('/:id',
-  param('id').escape().custom(async (id) => {
-    if (!(await Autor.findByPk(id))) {
-      Promise.reject(new Error(`Não existe autor com o id ${id}.`))
-    }
-  }),
+  param('id').isNumeric(),
   async (req, res) => {
     try {
       const errors = validationResult(req)
@@ -72,32 +65,41 @@ autorRouter.delete('/:id',
         return res.status(400).json({ errors: errors.array() })
       }
       const { id } = req.params
-      await deleteAutor(id)
-      return res.status(204)
+      const autor = await getAutor(id)
+      if (autor) {
+        await deleteAutor(id)
+        return res.status(204).json({})
+      }
+      throw new Error(`Não existe autor com o id ${id}.`)
     } catch (error) {
-      return res.status(400).json({ msg: error.messsage })
+      return res.status(400).json({ erro: error.message })
     }
   }
 )
 
 autorRouter.put('/',
-  body('autor_id').not().isEmpty().trim().escape().isNumeric(),
-  body('nome').not().isEmpty().trim().escape(),
-  body('email').not().isEmpty().trim().escape().custom(async (email, { req }) => {
-    const autor = await Autor.findOne({ where: { email } })
-    if (autor.cliete_id !== parseInt(req.body.autor_id)) {
-      Promise.reject(new Error('E-mail já está cadastrado para outro usuário.'))
-    }
-  }),
-  body('senha').not().isEmpty().trim().escape(),
+  body('autor_id').trim().escape().not().isEmpty().isNumeric(),
+  body('nome').trim().escape().not().isEmpty(),
+  body('email').not().isEmpty().trim().escape().isEmail(),
   body('telefone').not().isEmpty().trim().escape(),
-  body('endereco').not().isEmpty().trim().escape(),
   async (req, res) => {
     try {
-      const { autor } = req.body
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+      }
+      const autor = req.body
+      const autorExists = await getAutor(autor.autor_id)
+      if (!autorExists) {
+        throw new Error(`Não existe autor cadastrado com o autor_id ${autor.autor_id}`)
+      }
+      const found = await getAutorByEmail(autor.email)
+      if (!!found && found.autor_id !== parseInt(autor.autor_id)) {
+        throw new Error('E-mail já está cadastrado para outro autor.')
+      }
       return res.status(200).json(await updateAutor(autor))
     } catch (error) {
-      return res.status(400).json({ msg: error.messsage })
+      return res.status(400).json({ msg: error.message })
     }
   }
 )
