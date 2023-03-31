29dcb6a9d0efff9d923bd436218a528100ea5a03
ataua <ataua.doederlein@gmail.com>
2023-03-30 23:14:40 -0300

    [UPDATE] venda

--- src/repositories/venda.repository.js
+++ src/repositories/venda.repository.js
@@ -1,20 +1,32 @@
+/* eslint-disable camelcase */
+const Livro = require('../models/livro.model')
 const Venda = require('../models/venda.model')
+const Autor = require('../models/autor.model')
 
 const createVenda = async (venda) => await Venda.create(venda)
+
 const getVenda = async (vendaId) => await Venda.findByPk(vendaId)
+
 const getVendas = async () => await Venda.findAll({})
-const deleteVenda = async (vendaId) => await Venda.destroy({ where: { venda_id: vendaId } })
-const updateVenda = async (venda) => {
-  const { venda_id } = venda
-  delete venda.venda_id
-  await Venda.update(venda, { where: { venda_id } })
-  return await getVenda(venda.venda_id)
-}
+
+const getVendasByQuery = async (query) => await Venda.findAll({ where: query })
+
+const getVendasByAutor = async (autor_id) => await Venda.findAll({
+  include: {
+    model: Livro,
+    where: { autor_id },
+    include: {
+      model: Autor,
+      as: 'autor',
+      where: { autor_id }
+    }
+  }
+})
 
 module.exports = {
   createVenda,
   getVenda,
   getVendas,
-  deleteVenda,
-  updateVenda
+  getVendasByQuery,
+  getVendasByAutor
 }
--- src/routes/venda.route.js
+++ src/routes/venda.route.js
@@ -1,18 +1,41 @@
+/* eslint-disable camelcase */
 const { Router } = require('express')
+const { param, body, validationResult } = require('express-validator')
+
 const {
   createVenda,
   getVenda,
   getVendas,
-  deleteVenda,
-  updateVenda
+  getVendasByQuery,
+  getVendasByAutor
 } = require('../repositories/venda.repository')
-const { param, body, validationResult } = require('express-validator')
-const Venda = require('../models/venda.model')
+const { getCliente } = require('../repositories/cliente.repository')
+const {
+  getLivro,
+  updateLivro
+} = require('../repositories/livro.repository')
 
 const vendaRouter = Router()
 
 vendaRouter.get('/', async (req, res) => {
   try {
+    const autor_id = req.query.autor_id || req.query.autorId || null
+    if (autor_id) {
+      return res.status(200).json(await getVendasByAutor(autor_id))
+    }
+    const livro_id = req.query.livro_id || req.query.livroId || null
+    const cliente_id = req.query.cliente_id || req.query.clienteId || null
+    const query = {
+      ...(livro_id && { livro_id: parseInt(livro_id) }),
+      ...(cliente_id && { cliente_id: parseInt(cliente_id) })
+    }
+    if (Object.keys(query).includes('livro_id') || Object.keys(query).includes('cliente_id')) {
+      const result = await getVendasByQuery(query)
+      if (result) {
+        return res.status(200).json(result)
+      }
+      return res.status(404).json({ erro: `Não foram encontrados livros para os dados: ${JSON.stringify(query)} ` })
+    }
     return res.status(200).json(await getVendas())
   } catch (error) {
     return res.status(400).json({ erro: error.message })
@@ -36,66 +59,31 @@ vendaRouter.get('/:id',
 )
 
 vendaRouter.post('/',
-  body('nome').not().isEmpty().trim().escape(),
-  body('email').not().isEmpty().trim().escape().custom(async (email) => {
-    if (await Venda.findOne({ where: { email } })) {
-      Promise.reject(new Error('E-mail já está cadastrado.'))
-    }
-  }),
-  body('senha').not().isEmpty().trim().escape(),
-  body('telefone').not().isEmpty().trim().escape(),
-  body('endereco').not().isEmpty().trim().escape(),
+  body('data').not().isEmpty().trim().escape().isDate(),
+  body('cliente_id').not().isEmpty().isInt().escape(),
+  body('livro_id').not().isEmpty().isInt().escape(),
   async (req, res) => {
     try {
       const errors = validationResult(req)
       if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
       }
-      const { venda } = req.body
+      const venda = req.body
+      const cliente = await getCliente(venda.cliente_id)
+      if (!cliente) {
+        return res.status(400).json({ erro: `Cliente não encontrado com o cliente_id ${venda.cliente_id}` })
+      }
+      const livro = await getLivro(venda.livro_id)
+      if (!livro) {
+        return res.status(400).json({ erro: `Livro não encontrado com o livro_id ${venda.livro_id}` })
+      }
+      if (livro.estoque <= 0) {
+        return res.status(400).json({ erro: `Não há estoque disponível do livro com livro_id ${venda.livro_id}` })
+      }
+      --livro.estoque
+      await updateLivro(livro)
+      venda.valor = livro.valor
       return res.status(200).json(await createVenda(venda))
-    } catch (error) {
-      return res.status(400).json({ msg: error.message })
-    }
-  }
-)
-
-vendaRouter.delete('/:id',
-  param('id').escape().custom(async (id) => {
-    if (!(await Venda.findByPk(id))) {
-      Promise.reject(new Error(`Não existe venda com o id ${id}.`))
-    }
-  }),
-  async (req, res) => {
-    try {
-      const errors = validationResult(req)
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
-      }
-      const { id } = req.params
-      await deleteVenda(id)
-      return res.status(204)
-    } catch (error) {
-      return res.status(400).json({ msg: error.message })
-    }
-  }
-)
-
-vendaRouter.put('/',
-  body('venda_id').not().isEmpty().trim().escape().isNumeric(),
-  body('nome').not().isEmpty().trim().escape(),
-  body('email').not().isEmpty().trim().escape().custom(async (email, { req }) => {
-    const venda = await Venda.findOne({ where: { email } })
-    if (venda.cliete_id !== parseInt(req.body.venda_id)) {
-      Promise.reject(new Error('E-mail já está cadastrado para outro usuário.'))
-    }
-  }),
-  body('senha').not().isEmpty().trim().escape(),
-  body('telefone').not().isEmpty().trim().escape(),
-  body('endereco').not().isEmpty().trim().escape(),
-  async (req, res) => {
-    try {
-      const { venda } = req.body
-      return res.status(200).json(await updateVenda(venda))
     } catch (error) {
       return res.status(400).json({ msg: error.message })
     }
